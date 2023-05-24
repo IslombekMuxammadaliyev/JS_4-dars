68cc6790326e689d8786736abd4c39d793f9cb24
uzb077 <islombekmuxammadaliyev07@gmail.com>
2023-05-24 20:54:28 +0500

    Bu JavaScript kodlari

---
+++ js/main2.js
@@ -0,0 +1,91 @@
+
+
+let wrap = document.getElementsByTagName("div").namedItem("wrap");
+let from = document.getElementsByTagName("div").namedItem("from");
+let rasm = document.getElementsByTagName("img").namedItem("rasm");
+let item = document.getElementsByTagName("h5").namedItem("item");
+let item2 = document.getElementsByTagName("h5").namedItem("item2");
+let item4 = document.getElementsByTagName("h5").namedItem("item4");
+let item5 = document.getElementsByTagName("h5").namedItem("item5");
+let label1 = document.getElementsByTagName("label").namedItem("label1");
+let label2 = document.getElementsByTagName("label").namedItem("label2");
+let label3 = document.getElementsByTagName("label").namedItem("label3");
+let label4 = document.getElementsByTagName("label").namedItem("label4");
+let label5 = document.getElementsByTagName("label").namedItem("label5");
+let label6 = document.getElementsByTagName("label").namedItem("label6");
+let label7 = document.getElementsByTagName("label").namedItem("label7");
+let ism = document.getElementsByTagName("input").namedItem("ism");
+let familiya = document.getElementsByTagName("input").namedItem("familiya");
+let email = document.getElementsByTagName("input").namedItem("email");
+let parol = document.getElementsByTagName("input").namedItem("parol");
+let input1 = document.getElementsByTagName("input").namedItem("input1");
+let kun = document.getElementsByTagName("select").namedItem("kun");
+let oy = document.getElementsByTagName("select").namedItem("oy");
+let yil = document.getElementsByTagName("select").namedItem("yil");
+
+
+wrap.style.width = "1200px";
+wrap.style.margin = "80px auto";
+
+rasm.style.width = "400px";
+rasm.style.height = "100px";
+rasm.style.position = "absolute";
+rasm.style.top = "-90px";
+rasm.style.left = "25px";
+
+from.style.width = "450px";
+from.style.margin = "0 auto";
+from.style.padding = " 0.3rem 1.2rem";
+from.style.boxSizing = "border-box";
+from.style.boxShadow = "0 0 20px rgba(80,80,80,0.7)";
+from.style.background = "#ffffff";
+from.style.position = "relative";
+
+item.style.textAlign = "center";
+item.style.color = "#808080";
+
+ism.style.width = "85%";
+ism.style.marginBottom = "10px";
+ism.style.marginLeft = "10px";
+ism.style.padding = "0.7rem 1.1rem";
+ism.style.borderRadius = "4px";
+ism.style.textAlign = "center";
+
+familiya.style.width = "85%";
+familiya.style.marginBottom = "10px";
+familiya.style.marginLeft = "10px";
+familiya.style.padding = "0.7rem 1.1rem";
+familiya.style.borderRadius = "4px";
+familiya.style.textAlign = "center";
+
+email.style.width = "85%";
+email.style.marginBottom = "10px";
+email.style.marginLeft = "10px";
+email.style.padding = "0.7rem 1.1rem";
+email.style.borderRadius = "4px";
+email.style.textAlign = "center";
+
+parol.style.width = "85%";
+parol.style.marginBottom = "10px";
+parol.style.marginLeft = "10px";
+parol.style.padding = "0.7rem 1.1rem";
+parol.style.borderRadius = "4px";
+parol.style.textAlign = "center";
+
+input1.style.width = "85%";
+input1.style.marginBottom = "10px";
+input1.style.marginLeft = "10px";
+input1.style.padding = "0.7rem 1.1rem";
+input1.style.borderRadius = "4px";
+input1.style.textAlign = "center";
+
+kun.style.width = "30%";
+kun.style.padding = "0.7rem 1.1rem";
+
+
+
+
+
+
+
+
